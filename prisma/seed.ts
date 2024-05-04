86f9637d559373f5e1680f67f28d9cc75d34d42c
sherzock <jacobo.galofre@gmail.com>
2024-05-04 23:45:33 +0200

    Separated teachers and students into 2 different tables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,212 +2,187 @@ import { PrismaClient } from "@prisma/client";
 export const db = new PrismaClient();
 
 async function main() {
-  const student1 = await db.person.create({
+  const student1 = await db.student.create({
     data: {
       firstName: "Jacob",
       lastName: "Galofre Calbeto",
       email: "jacobo.galofre@test.com",
-      role: "STUDENT",
     },
   });
 
-  const student2 = await db.person.create({
+  const student2 = await db.student.create({
     data: {
       firstName: "Bilbo",
       lastName: "Baggins",
       email: "bilbo.baggins@test.com",
-      role: "STUDENT",
     },
   });
-  const student3 = await db.person.create({
+  const student3 = await db.student.create({
     data: {
       firstName: "Frodo",
       lastName: "Baggins",
       email: "frodo.baggins@test.com",
-      role: "STUDENT",
     },
   });
-  const student4 = await db.person.create({
+  const student4 = await db.student.create({
     data: {
       firstName: "Samwise",
       lastName: "Gamgee",
       email: "samwise.gamgee@test.com",
-      role: "STUDENT",
     },
   });
-  const student5 = await db.person.create({
+  const student5 = await db.student.create({
     data: {
       firstName: "Meriadoc",
       lastName: "Brandybuck",
       email: "meriadoc.brandybuck@test.com",
-      role: "STUDENT",
     },
   });
-  const student6 = await db.person.create({
+  const student6 = await db.student.create({
     data: {
       firstName: "Gandalf",
       lastName: "The Grey",
       email: "gandalf.thegrey@test.com",
-      role: "STUDENT",
     },
   });
-  const student7 = await db.person.create({
+  const student7 = await db.student.create({
     data: {
       firstName: "Aragorn",
       lastName: "Telcontar",
       email: "aragorn.telcontar@test.com",
-      role: "STUDENT",
     },
   });
-  const student8 = await db.person.create({
+  const student8 = await db.student.create({
     data: {
       firstName: "Legolas",
       lastName: "Greenleaf",
       email: "legolas.greenleaf@test.com",
-      role: "STUDENT",
     },
   });
-  const student9 = await db.person.create({
+  const student9 = await db.student.create({
     data: {
       firstName: "Gimli",
       lastName: "son of Gloin",
       email: "gimli.sonofgloin@test.com",
-      role: "STUDENT",
     },
   });
-  const student10 = await db.person.create({
+  const student10 = await db.student.create({
     data: {
       firstName: "Boromir",
       lastName: "son of Denethor",
       email: "boromir.sonofdebethor@test.com",
-      role: "STUDENT",
     },
   });
-  const student11 = await db.person.create({
+  const student11 = await db.student.create({
     data: {
       firstName: "Lobelia",
       lastName: "Sacksville Baggins",
       email: "lobelia.sacksville@test.com",
-      role: "STUDENT",
     },
   });
-  const student12 = await db.person.create({
+  const student12 = await db.student.create({
     data: {
       firstName: "Otho",
       lastName: "Sacksville Baggins",
       email: "Otho.Sacksville@test.com",
-      role: "STUDENT",
     },
   });
-  const student13 = await db.person.create({
+  const student13 = await db.student.create({
     data: {
       firstName: "Thorin",
       lastName: "Oakenshield",
       email: "thorin.oakenshield@test.com",
-      role: "STUDENT",
     },
   });
-  const student14 = await db.person.create({
+  const student14 = await db.student.create({
     data: {
       firstName: "Belladonna",
       lastName: "Took",
       email: "belladonna.took@test.com",
-      role: "STUDENT",
     },
   });
-  const student15 = await db.person.create({
+  const student15 = await db.student.create({
     data: {
       firstName: "Tom",
       lastName: "Bombadil",
       email: "tom.bombadil@test.com",
-      role: "STUDENT",
     },
   });
-  const student16 = await db.person.create({
+  const student16 = await db.student.create({
     data: {
       firstName: "Berylla",
       lastName: "Boffin",
       email: "berylla.boffin@test.com",
-      role: "STUDENT",
     },
   });
-  const student17 = await db.person.create({
+  const student17 = await db.student.create({
     data: {
       firstName: "Linda",
       lastName: "Baggins",
       email: "linda.baggins@test.com",
-      role: "STUDENT",
     },
   });
-  const student18 = await db.person.create({
+  const student18 = await db.student.create({
     data: {
       firstName: "Primula",
       lastName: "Brandybuck",
       email: "primula.brandybuck@test.com",
-      role: "STUDENT",
     },
   });
 
-  const student19 = await db.person.create({
+  const student19 = await db.student.create({
     data: {
       firstName: "John",
       lastName: "Ronald Reuel Tolkien",
       email: "JRR.tolkien@test.com",
-      role: "STUDENT",
     },
   });
 
-  const student20 = await db.person.create({
+  const student20 = await db.student.create({
     data: {
       firstName: "Edith",
       lastName: "Mary Tolkien",
       email: "edith.tolkien@test.com",
-      role: "STUDENT",
     },
   });
 
-  const Teacher1 = await db.person.create({
+  const Teacher1 = await db.teacher.create({
     data: {
       firstName: "Harry",
       lastName: "Potter",
       email: "harry.potter@test.com",
-      role: "TEACHER",
     },
   });
 
-  const Teacher2 = await db.person.create({
+  const Teacher2 = await db.teacher.create({
     data: {
       firstName: "Hermione",
       lastName: "Granger",
       email: "hermione.granger@test.com",
-      role: "TEACHER",
     },
   });
 
-  const Teacher3 = await db.person.create({
+  const Teacher3 = await db.teacher.create({
     data: {
       firstName: "Ronald",
       lastName: "Weasly",
       email: "ronald.weasly@test.com",
-      role: "TEACHER",
     },
   });
 
-  const Teacher4 = await db.person.create({
+  const Teacher4 = await db.teacher.create({
     data: {
       firstName: "Albus",
       lastName: "Dumbeldore",
       email: "albus.dumbelodre@test.com",
-      role: "TEACHER",
     },
   });
 
-  const Teacher5 = await db.person.create({
+  const Teacher5 = await db.teacher.create({
     data: {
       firstName: "Severus",
       lastName: "Snape",
       email: "severus.snape@test.com",
-      role: "TEACHER",
     },
   });
 
